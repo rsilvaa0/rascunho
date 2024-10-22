98fb0d9409e6b75f6421e49adc104657397280f6
rsilvaa0 <142804423+rsilvaa0@users.noreply.github.com>
2024-10-22 11:03:11 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,27 @@
+criaCartao(
+    //categoria,
+    'Programação'
+    //pergunta,
+    'O que é JavaScript'
+    //resposta,
+    'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    //categoria,
+    'Programação'
+    //pergunta,
+    'O que é JavaScript'
+    //resposta,
+    'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    //categoria,
+    'Programação'
+    //pergunta,
+    'O que é JavaScript'
+    //resposta,
+    'O JavaScript é uma linguagem de programação'
+)
+
